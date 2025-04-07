295ebe6fe38f78c952542411e987a6a373c4cdb3
Markus Battarbee <markus.battarbee@gmail.com>
2025-04-07 15:08:37 +0300

    Switch field solver to use minmod limiter

--- fieldsolver/fs_limiters.h
+++ fieldsolver/fs_limiters.h
@@ -101,7 +101,9 @@ template<typename T> inline T vanLeer(const T& left,const T& cent,const T& right
 
 
 template<typename T> inline T limiter(const T& left,const T& cent,const T& rght) {
-   return vanLeer(left,cent,rght);
+   //return vanLeer(left,cent,rght);
+   // Attempt to use more diffusive limiter in order to curtail field solver oscillations
+   return minmod(left,cent,rght);
 }
 
 /*! Select the limiter to be used in the field solver. */
